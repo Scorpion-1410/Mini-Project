c3b0787454de9c9576d83dd6083052982d047e47
Tanishq1410 <tanishq.tripathi_cs18@gla.ac.in>
2021-03-28 21:00:17 +0530

    File Updated

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from tkinter import *
 from tkinter.ttk import *
-
-# import pywhatkit
-
-from whatsapp import user_interface
+from whatsapp import user_interface as wui
+from gmail import user_interface as gui
 
 main_screen = None
 
@@ -15,7 +13,8 @@ def main_account_screen():
     main_screen.title("Account Login")
     Label(text="Select Your Choice").pack()
     Label(text="").pack()
-    Button(text="WhatsApp", command=user_interface).pack()
+    Button(text="WhatsApp", command=wui).pack()
+    Button(text="Gmail", command=gui).pack()
 
     main_screen.mainloop()
 

3b9c77b53c43abecf2355f009b0af3907821810f
Prashant Asthana <prashant.asthana_cs18@gla.ac.in>
2021-04-07 09:09:29 +0530

    initialize variable

---
+++ youtube.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import StringVar
+from tkinter.ttk import *
+import pywhatkit
+from pytube import YouTube
+
+youtube_screen = None
+youtube_search = None
+youtube_link = None
+main_screen = None
